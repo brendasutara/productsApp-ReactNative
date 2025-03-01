2ba53983ed647655a9086d398b9c95465956d1a8
Brenda Sutara <sutarabrendagmail.com>
2025-03-01 15:53:39 -0300

    Diseño de login

--- src/presentation/screens/auth/LoginScreen.tsx
+++ src/presentation/screens/auth/LoginScreen.tsx
@@ -1,9 +1,48 @@
-import { Text, View } from "react-native"
+import {Input, Layout, Text, Button} from '@ui-kitten/components';
+import {useWindowDimensions} from 'react-native';
+import {ScrollView} from 'react-native-gesture-handler';
 
 export const LoginScreen = () => {
-    return (
-        <View>
-            <Text>LoginScreen</Text>
-        </View>
-    )
-}
+  const {height} = useWindowDimensions();
+  return (
+    <Layout style={{flex: 1}}>
+      <ScrollView style={{marginHorizontal: 40}}>
+        <Layout style={{paddingTop: height * 0.35}}>
+          <Text category="h1">Ingresar</Text>
+          <Text>Por favor, ingrese sus credenciales para continuar</Text>
+        </Layout>
+        <Layout style={{marginTop: 20}}>
+          <Input
+            placeholder="Correo electrónico"
+            keyboardType="email-address"
+            autoCapitalize="none"
+            style={{marginBottom: 10}}
+          />
+          <Input
+            placeholder="Contraseña"
+            autoCapitalize="none"
+            secureTextEntry
+            style={{marginBottom: 10}}
+          />
+        </Layout>
+        <Layout style={{height: 20}} />
+        <Layout>
+          <Button onPress={() => {}}>Ingresar</Button>
+        </Layout>
+        <Layout style={{height: 50}} />
+        <Layout
+          style={{
+            alignItems: 'center',
+            flexDirection: 'row',
+            justifyContent: 'center',
+            gap: 10,
+          }}>
+          <Text>¿No tienes una cuenta?</Text>
+          <Text status="primary" category="s1" onPress={() => {}}>
+            Crea una aquí
+          </Text>
+        </Layout>
+      </ScrollView>
+    </Layout>
+  );
+};
